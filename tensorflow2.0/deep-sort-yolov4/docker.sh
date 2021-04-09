3e8a2adf57b038d29af6f0e36c10a9de20bc60bd
aj1m0n <surpasstdu@gmail.com>
2021-04-09 16:08:10 +0900

    update

--- tensorflow2.0/deep-sort-yolov4/docker.sh
+++ tensorflow2.0/deep-sort-yolov4/docker.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -v /home/jetson-iwai/workspace/:/workspace/ --device /dev/video0:/dev/video0 --runtime nvidia --network host jetson-iwai/l4-ml-ten2:v2.0
+sudo docker run -it -v ~/workspace/:/workspace/ --device /dev/video0:/dev/video0 --runtime nvidia --network host aj1m0n/deep_sort:latest 
