83a0eef50265bc5b63b8e205d3076bf1b4ae4041
aj1m0n <surpasstdu@gmail.com>
2021-07-18 17:51:37 +0900

    fix dockerrun

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,1 +1,1 @@
-docker run -it -v ~/workspace/Deep_Sort/:/Deep_Sort/ -e IP=`ip -f inet -o addr show wlan0|cut -d\  -f 7 | cut -d/ -f 1` --runtime nvidia --network host --name deepsort deepsort
+docker run -it -v ~/workspace/Deep_Sort/:/Deep_Sort/ -e IP=`ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1` -restart=always --runtime nvidia --network host --name deepsort deepsort
