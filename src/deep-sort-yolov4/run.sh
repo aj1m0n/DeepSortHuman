854ef22468a007630d93322cd0bc98facfa22690
aj1m0n <surpasstdu@gmail.com>
2021-07-29 06:58:44 +0900

    update

--- src/deep-sort-yolov4/run.sh
+++ src/deep-sort-yolov4/run.sh
@@ -1,1 +1,1 @@
-python3 ./main.py --cam_cmd "/nphMotionJpeg?Resolution=1280x720&Quality=Standard&Framerate=20" --key 'jp.chiba.kashiwa.kashiwanoha.25.sensor.' --ipaddress $IP  -ipcamera_flag --json_path "/json/"
+python3 ./main.py -jpegmode --cam_cmd "/nphMotionJpeg?Resolution=1280x720&Quality=Standard&Framerate=10" --key 'jp.chiba.kashiwa.kashiwanoha.25.sensor.' --ipaddress $IP  -ipcamera_flag --json_path "/json/"
