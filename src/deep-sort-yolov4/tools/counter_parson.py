c3694bbf1080c65a8bb7535789ab09e881edcdbf
aj1m0n <surpasstdu@gmail.com>
2021-08-18 03:16:58 +0900

    update

--- src/deep-sort-yolov4/tools/counter_parson.py
+++ src/deep-sort-yolov4/tools/counter_parson.py
@@ -3,10 +3,10 @@ class CountParson:
         self.parson_id_list = []
         self.first_position_list = []
         self.end_position_list = []
-        self.first_id_list = []
+        self.first_id_list = [0]
         self.count_list = []
         self.t_count_list = []
-        self.f_count_list = [[0]]
+        self.f_count_list = []
         
 
     def positions(self, _parson_dict, _latest_track_id):
@@ -18,12 +18,12 @@ class CountParson:
         for _track_id in self.first_id_list:
             print('positions')
             if not _parson_dict.get(str(_track_id)):
-                if self.first_position_list[_track_id][0] - self.end_position_list[_track_id][0] > 0:
-                    self.t_count_list.append(_track_id)
+                if self.first_position_list[int(_track_id)][0] - self.end_position_list[int(_track_id)][0] > 0:
+                    self.t_count_list.append(int(_track_id))
                 else:
-                    self.f_count_list.append(_track_id)
+                    self.f_count_list.append(int(_track_id))
             else:
-                self.end_position_list[_track_id] = _parson_dict[str(_track_id)]
+                self.end_position_list[int(_track_id)] = _parson_dict[str(_track_id)]
         
         return len(self.t_count_list), len(self.f_count_list)
 
