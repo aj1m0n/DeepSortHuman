e97e2f9f7a59c1b3b37f675e874a6df36d6466fe
aj1m0n <surpasstdu@gmail.com>
2021-08-20 10:13:11 +0900

    fixed

--- src/deep-sort-yolov4/tools/counter_parson.py
+++ src/deep-sort-yolov4/tools/counter_parson.py
@@ -39,7 +39,7 @@ class CountParson:
                 _fy = int(self.first_position_list[_key][1] + (self.first_position_list[_key][3] - self.first_position_list[_key][1]) / 2)
                 _ex = int(self.end_position_list[_key][0] + (self.end_position_list[_key][2] - self.end_position_list[_key][0]) / 2)
                 _ey = int(self.end_position_list[_key][1] + (self.end_position_list[_key][3] - self.end_position_list[_key][1]) / 2)
-                if math.sqrt(pow(_ex -_fx, 2) + pow(_ey - _fy, 2)) > 500:
+                if math.sqrt(pow(_ex -_fx, 2) + pow(_ey - _fy, 2)) > 250:
                     self.ids_position_dict["id"] = _key
                     if _ex -_fx > 0:
                         self.ids_position_dict["direction"] = "L"
